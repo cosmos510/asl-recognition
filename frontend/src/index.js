1b885d285bdbd4ff0ee89c4742b4d9f212de1331
cosmos510 <maxime.martin510.com>
2024-09-04 11:05:30 +0200

    chore: Add Dockerfiles and configuration for Django backend and React frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Handy App</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

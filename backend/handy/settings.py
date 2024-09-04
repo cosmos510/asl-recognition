8a38c811582046746fdf3eb0094c8850bec9ea8a
cosmos510 <maxime.martin510.com>
2024-09-04 17:26:04 +0200

    perfectionning the db

--- backend/handy/settings.py
+++ backend/handy/settings.py
@@ -120,7 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# URL to use when referring to static files (e.g., in templates)
+STATIC_URL = '/static/'
+
+# Absolute path to the directory where collectstatic will collect static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories to check for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

02770e582b5a200fa9c78e85a400ed44b1ad54c6
cosmos510 <maxime.martin510.com>
2024-09-07 09:58:36 +0200

    feat: Add libgl1-mesa-glx package to Dockerfile for backend

--- backend/handy/settings.py
+++ backend/handy/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'handy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'public')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

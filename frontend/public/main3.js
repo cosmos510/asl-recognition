35f788df6d4a2b03a25b9149c16efe5b8f28aac0
cosmos510 <maxime.martin510.com>
2024-09-11 10:25:31 +0200

    Add footer container in index.html

--- frontend/public/main3.js
+++ frontend/public/main3.js
@@ -7,4 +7,10 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('header-container').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
+        fetch('/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
 });

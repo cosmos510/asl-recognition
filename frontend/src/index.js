7a623f0575743ea414a349b3c3d9b9845893e1b1
cosmos510 <maxime.martin510.com>
2024-09-12 16:41:32 +0200

    Refactor views.py and index.js files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
-
-function App() {
-    return (
-        <div>
-            <h1>Welcome to Handy App</h1>
-        </div>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));

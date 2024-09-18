30009b326caedd4a85d1588ad752fc59df9d221f
cosmos510 <maxime.martin510.com>
2024-09-18 11:27:25 +0200

    Refactor frontend add_feedback.js to load header and footer

--- frontend/public/add_feedback.js
+++ frontend/public/add_feedback.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    // Load header and footer
     fetch('/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
-        fetch('/footer.html')
+    
+    fetch('/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
         })
         .catch(error => console.error('Error loading footer:', error));
 
-    const form = document.getElementById("feedback-form");
-   
+
 });

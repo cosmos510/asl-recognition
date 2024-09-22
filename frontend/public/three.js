60b1e8cbb44c7d88989bf899376c7676c237c8a7
cosmos510 <maxime.martin510.com>
2024-09-22 17:44:14 +0200

    Refactor three.html and three.js files, update meta tags and title

--- frontend/public/three.js
+++ frontend/public/three.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const scene = new THREE.Scene();
-
+    
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0, 5);
-
+    
     const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('three-canvas'), antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Set initial size of the renderer
+    const footerHeight = 100; // Adjust based on your footer height
+    renderer.setSize(window.innerWidth, window.innerHeight - footerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-
+    
     camera.lookAt(cube.position);
-
+    
     function animate() {
         requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
@@ -25,30 +28,11 @@ document.addEventListener('DOMContentLoaded', function () {
     function onWindowResize() {
         const width = window.innerWidth;
         const height = window.innerHeight;
-        console.log('Window resized:', width, height);
-
-        renderer.setSize(width, height);
-
-
-        camera.aspect = width / height;
+        renderer.setSize(width, height - footerHeight);
+        camera.aspect = width / (height - footerHeight);
         camera.updateProjectionMatrix();
     }
 
-
     window.addEventListener('resize', onWindowResize);
-
     animate();
-    fetch('/header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading header:', error));
-  
-    fetch('/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
 });

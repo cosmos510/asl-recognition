473a076a727fca44b013a5f41961ace946221d36
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2024-10-28 18:33:41 +0100

    cleaning up

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Frame, Prediction, Feedback, TrainingTask
+from .models import User, Feedback
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
@@ -24,26 +24,9 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'username', 'email', 'created_at')
 
 
-@admin.register(Frame)
-class FrameAdmin(admin.ModelAdmin):
-    list_display = ('frame_id', 'user', 'upload_time', 'image_path')
-
-
-@admin.register(Prediction)
-class PredictionAdmin(admin.ModelAdmin):
-    list_display = (
-        'prediction_id',
-        'frame',
-        'predicted_class',
-        'confidence',
-        'timestamp')
-
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('feedback_id', 'user', 'comment', 'timestamp')
 
 
-@admin.register(TrainingTask)
-class TrainingTaskAdmin(admin.ModelAdmin):
-    list_display = ('task_id', 'user', 'start_time', 'status', 'progress')
--- backend/app/models.py
+++ backend/app/models.py
@@ -36,25 +36,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.username
 
-class Frame(models.Model):
-    frame_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    upload_time = models.DateTimeField(auto_now_add=True)
-    image_path = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"Frame {self.frame_id} by {self.user.username}"
-
-class Prediction(models.Model):
-    prediction_id = models.AutoField(primary_key=True)
-    frame = models.ForeignKey(Frame, on_delete=models.CASCADE)
-    predicted_class = models.CharField(max_length=255)
-    confidence = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Prediction {self.prediction_id} for Frame {self.frame.frame_id}"
-
 class Feedback(models.Model):
     feedback_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -64,13 +45,3 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback {self.feedback_id} by {self.user.username}"
 
-
-class TrainingTask(models.Model):
-    task_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=50)
-    progress = models.FloatField()
-
-    def __str__(self):
-        return f"TrainingTask {self.task_id} by {self.user.username}"
